b26f151c492dea0e3e8831f0bc2b44dd7b906299
Mark Slorach <hello@markslorach.com>
2026-05-20 17:15:58 +0100

    Back home link styling in not-found.tsx

--- app/(site)/not-found.tsx
+++ app/(site)/not-found.tsx
@@ -5,11 +5,11 @@ export default function NotFound() {
   return (
     <>
       <Link
-        href="/"
-        className="text-muted-foreground flex items-center text-sm"
+        href="/writing"
+        className="text-muted-foreground group hover:text-foreground flex items-center text-sm transition-colors duration-200 ease-in-out"
       >
-        <ArrowLeft className="mr-1 size-4" />
-        Return home
+        <ArrowLeft className="mr-1 size-4 transition-transform duration-200 ease-in-out group-hover:-translate-x-0.5 group-hover:text-blue-400" />
+        Back home
       </Link>
       <p className="text-muted-foreground my-10">Page not found.</p>
     </>
