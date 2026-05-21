ae14cd270dc92f849f602441e8c7ca203edb1d8f
Mark Slorach <hello@markslorach.com>
2026-05-21 13:02:28 +0100

    Fix target in footer GitHub link

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
 
       <Link
         href="https://github.com/markslorach/www.git"
-        target="black"
+        target="blank"
         className="hover:text-muted-foreground hidden transition-colors duration-200 ease-in-out sm:flex"
       >
         <FaGithub className="size-4.5" />
