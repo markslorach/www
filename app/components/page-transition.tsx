95ae4668beb56c1085bb08be1215d3532bb74667
Mark Slorach <hello@markslorach.com>
2026-05-21 22:21:58 +0100

    Add smooth scrolling globals.css and implement sroll to top in pagetranstion.tsx to fix y axis glitch on refresh

--- app/components/page-transition.tsx
+++ app/components/page-transition.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "motion/react";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function PageTransition({
   children,
@@ -8,6 +9,11 @@ export default function PageTransition({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, []);
 
   return (
     <motion.div
