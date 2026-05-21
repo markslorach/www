ef3b866a0f610c35e99d1f6cd88c8c414e8f199b
Mark Slorach <hello@markslorach.com>
2026-05-21 13:50:33 +0100

    Update footer

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -5,20 +5,23 @@ export default function Footer() {
   return (
     <footer className="text-muted-foreground/70 flex items-center justify-between text-[13px] leading-none">
       <p>
-        Mark Slorach · 2026 · Inspired by{" "}
-        <Link
-          href="https://deadsimplesites.com"
-          target="_blank"
-          className="hover:text-muted-foreground"
-        >
-          Dead Simple Sites
-        </Link>
+        Mark Slorach · 2026{" "}
+        <span className="hidden md:inline">
+          · Inspired by{" "}
+          <Link
+            href="https://deadsimplesites.com"
+            target="_blank"
+            className="hover:text-muted-foreground"
+          >
+            Dead Simple Sites
+          </Link>
+        </span>
       </p>
 
       <Link
         href="https://github.com/markslorach/www.git"
         target="blank"
-        className="hover:text-muted-foreground hidden transition-colors duration-200 ease-in-out sm:flex"
+        className="hover:text-muted-foreground transition-colors duration-200 ease-in-out"
       >
         <FaGithub className="size-4.5" />
       </Link>
