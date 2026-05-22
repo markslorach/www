be4e8caca51d442730dfcbf17f539a8e6316e059
Mark Slorach <hello@markslorach.com>
2026-05-22 17:27:39 +0100

    Remove transition classes from layout and sidebar

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,7 +9,7 @@ export default function SiteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto flex min-h-svh w-full max-w-4xl gap-18 px-6 transition-all duration-200 ease-in-out md:px-4 lg:gap-26">
+    <div className="mx-auto flex min-h-svh w-full max-w-4xl gap-18 px-6 md:px-4 lg:gap-26">
       <Sidebar />
       <div className="flex min-w-0 flex-1 flex-col pb-10 md:pt-10">
         <MobileHeader />
--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ import Navigation from "./navigation";
 
 export default function Sidebar() {
   return (
-    <aside className="sticky top-0 hidden h-screen w-56 flex-col overflow-y-scroll py-10 pr-4 transition-all duration-200 ease-in-out md:flex lg:pr-0">
+    <aside className="sticky top-0 hidden h-screen w-56 flex-col overflow-y-scroll py-10 pr-4 md:flex lg:pr-0">
       <div className="flex-1">
         <Header />
         <Navigation />
