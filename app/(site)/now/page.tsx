89e968e8964218dff7e56c7707688426a066a2a0
Mark Slorach <hello@markslorach.com>
2026-05-25 11:22:06 +0100

    Create link group for navigation. Add /now page. Update prose styling.

--- app/(site)/now/page.tsx
+++ app/(site)/now/page.tsx
@@ -7,17 +7,7 @@ export default function UsesPage() {
         /now
       </h1>
 
-      <p className="text-pretty">
-        Following the tend of the{" "}
-        <Link
-          href="https://nownownow.com/about"
-          target="_blank"
-          className="text-foreground underline decoration-[#0892d0] decoration-dotted decoration-1 underline-offset-4 hover:decoration-solid"
-        >
-          now
-        </Link>{" "}
-        page. This is what I am currently focused on.
-      </p>
+      <p className="text-pretty">Coming soon...</p>
     </section>
   );
 }
--- app/components/sidebar/navigation.tsx
+++ app/components/sidebar/navigation.tsx
@@ -3,38 +3,47 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-export const navLinks = [
+export const primaryLinks = [
   { label: "home", href: "/" },
   { label: "about", href: "/about" },
   { label: "writing", href: "/writing" },
-  { label: "uses", href: "/uses" },
-  // { label: "now", href: "/now" },
 ];
+
+export const metaLinks = [
+  { label: "uses", href: "/uses" },
+  { label: "now", href: "/now" },
+];
+
+const linkGroups = [primaryLinks, metaLinks];
 
 export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex flex-1 flex-col gap-2 md:gap-1 md:py-12">
-      {navLinks.map((link) => (
-        <Link
-          href={link.href}
-          key={link.href}
-          className={cn(
-            "group text-muted-foreground hover:text-foreground flex text-xl items-center gap-1 md:text-sm transition-colors duration-200 ease-in-out",
-            link.href === pathname && "text-foreground",
-          )}
-        >
-          <span
-            className={cn(
-              "w-2.5 text-base font-medium text-[#0892d0] opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100",
-              link.href === pathname && "opacity-100",
-            )}
-          >
-            /
-          </span>
-          {link.label}
-        </Link>
+    <nav className="flex flex-1 flex-col gap-5 md:py-12">
+      {linkGroups.map((group, i) => (
+        <div key={i} className="flex flex-col gap-2 md:gap-1">
+          {group.map((link) => (
+            <Link
+              href={link.href}
+              key={link.href}
+              className={cn(
+                "group text-muted-foreground hover:text-foreground flex text-xl items-center gap-1 md:text-sm transition-colors duration-200 ease-in-out",
+                link.href === pathname && "text-foreground",
+              )}
+            >
+              <span
+                className={cn(
+                  "w-2.5 text-base font-medium text-[#0892d0] opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100",
+                  link.href === pathname && "opacity-100",
+                )}
+              >
+                /
+              </span>
+              {link.label}
+            </Link>
+          ))}
+        </div>
       ))}
     </nav>
   );
